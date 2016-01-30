51715342ff1469e0f6c9e26d63ad22b2ea1487b1
Rafal <rafael.orlowsky@gmail.com>
2016-01-30 20:48:55 +0100

    clean2

--- public/js/rooms.js
+++ public/js/rooms.js
@@ -1,7 +1,3 @@
-/**
- * Adds employee deletion events to all elements with class deleteEquipment.
- * Elements need attribute rel=[employee id].
- */
 $(document).ready(function() {
     /**
      * @param {int} id
@@ -24,17 +20,6 @@ $(document).ready(function() {
         .fail(function() {
             alert("Sorry - the request did not work.");
         });
-        /*$.ajax({
-            type: "POST",
-            url: "http://localhost/zend/RestApplication/public/delete_employee",
-            data: {'id': id},
-            success: function(msg){
-                  alert( "Data Saved: " + msg );
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-               alert("some error");
-            }
-          });*/
     }
     
     
